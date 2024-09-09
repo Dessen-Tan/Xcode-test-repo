5b282af87e6925088808aca1575e93e72119e1b4
Dessen Tan <dessen_arwin_tan_ahn@s2024.ssts.edu.sg>
2024-09-09 15:59:49 +0800

    Initial Commit

---
+++ Github test/Github_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  Github_testApp.swift
+//  Github test
+//
+//  Created by Dessen Tan on 9/9/24.
+//
+
+import SwiftUI
+
+@main
+struct Github_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
