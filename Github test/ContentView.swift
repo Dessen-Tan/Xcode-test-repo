6008df5e2d971f8a03884050f5fdf979e3fb290d
Dessen Tan <dessen_arwin_tan_ahn@s2024.ssts.edu.sg>
2024-09-09 16:11:44 +0800

    Change hello to hi, added spacers, another hi world and an image

--- Github test/ContentView.swift
+++ Github test/ContentView.swift
@@ -9,11 +9,17 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
+        VStack{
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("hi, world!")
+            Spacer()
+            Image(systemName: "square.and.arrow.up")
+                .foregroundColor(.blue)
+                .frame(width: 100, height: 100)
+            Spacer()
+            Text("hi, world")
         }
         .padding()
     }
